3bc9a9c96a6d1a0f0cb33b5a584874627d078df9
GitSRealpe <msn-realpe@hotmail.com>
2021-04-25 12:08:47 -0500

    clean edit

--- taller2/scripts/true_odom.py
+++ taller2/scripts/true_odom.py
@@ -21,28 +21,18 @@ def get_odom(modelos):
     index=modelos.name.index(obj)
     pose=modelos.pose[index]
     vel=modelos.twist[index]
-    # print("posicion:\n",pose)
-    # print("velocidad:\n",vel)
-    # h.stamp = rospy.Time.now()
-    # h.frame_id="odom"
 
-    # odom_trans.header=h
-    # odom_trans.child_frame_id="base_footprint"
     odom_trans.transform.translation.x=pose.position.x;
     odom_trans.transform.translation.y=pose.position.y;
     odom_trans.transform.translation.z=0;
     odom_trans.transform.rotation=pose.orientation;
-    # tb.sendTransformMessage(odom_trans)
 
-    # odom_msg.header=h
-    # odom_msg.child_frame_id="base_footprint"
     odom_msg.pose.pose.position.x=pose.position.x;
     odom_msg.pose.pose.position.y=pose.position.y;
     odom_msg.pose.pose.position.z=0
     odom_msg.pose.pose.orientation=pose.orientation;
     odom_msg.twist.twist.linear.x=vel.linear.x;odom_msg.twist.twist.linear.y=vel.linear.y;
     odom_msg.twist.twist.angular.z=vel.angular.z;
-    # odom_pub.publish(odom_msg)
 
 def pub_odom():
     h.frame_id="odom"
@@ -51,7 +41,7 @@ def pub_odom():
     odom_msg.header=h
     odom_msg.child_frame_id="base_footprint"
     while not rospy.is_shutdown():
-        print(odom_msg)
+        # print(odom_msg)
         h.stamp = rospy.Time.now()
         tb.sendTransformMessage(odom_trans)
         odom_pub.publish(odom_msg)
